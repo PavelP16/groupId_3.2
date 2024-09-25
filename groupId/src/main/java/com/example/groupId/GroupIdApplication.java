1bdc6e3afa04ceba2015a6b88fa7c0573e38e0dd
Pavel <pavelpulle1@gmail.co>
2024-09-25 21:51:46 +0300

    Initial commit

---
+++ groupId/src/main/java/com/example/groupId/GroupIdApplication.java
@@ -0,0 +1,13 @@
+package com.example.groupId;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupIdApplication.class, args);
+	}
+
+}
---
+++ groupId/src/test/java/com/example/groupId/GroupIdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groupId;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
